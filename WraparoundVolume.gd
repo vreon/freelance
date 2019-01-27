abd8311718887d008068f142b9f194c02ef61d93
Jesse Dubay <jesse@jessedubay.com>
2019-01-26 19:26:13 -0800

    Better WraparoundVolume implementation

--- WraparoundVolume.gd
+++ WraparoundVolume.gd
@@ -2,6 +2,20 @@ extends Area2D
 
 export var offset = -400
 
-func _on_body_entered(body):
-	if sign(body.velocity.x) != sign(offset):
-		body.position.x += offset
+export(bool) var wrap_vertical = false
+export(bool) var wrap_horizontal = true
+
+onready var shape = $CollisionShape2D.shape
+
+func _on_WraparoundVolume_body_exited(body):
+	if wrap_horizontal and body.position.x <= position.x - shape.extents.x:
+		body.position.x = position.x + shape.extents.x - 1
+		
+	if wrap_horizontal and body.position.x >= position.x + shape.extents.x:
+		body.position.x = position.x - shape.extents.x + 1
+		
+	if wrap_vertical and body.position.y <= position.y - shape.extents.y:
+		body.position.y = position.y + shape.extents.y - 1
+		
+	if wrap_vertical and body.position.y >= position.y + shape.extents.y:
+		body.position.y = position.y - shape.extents.y + 1
