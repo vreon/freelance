acdf9eae293ae6eb0aa092d395a2f35afdc7e87b
Jesse Dubay <jesse@jessedubay.com>
2019-01-27 15:17:20 -0800

    Remove water shader for now
    Doesn't work in HTML export

--- levels/Water.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-tool
-
-func _ready():
-	# TODO: Uhhh I guess shaders are shared, so setting param here is useless
-	$ColorRect.get_material().set_shader_param("height", $ColorRect.rect_size.y)
